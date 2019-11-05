457f0831ad1bf6e36175db5c12ffb97875991e46
cutelua <33216131+cutelua@users.noreply.github.com>
2019-11-05 16:19:51 +0800

    set deadline from ctx on Dial

--- VPN/vpnservice_support.go
+++ VPN/vpnservice_support.go
@@ -289,6 +289,7 @@ func (d *ProtectedDialer) fdConn(ctx context.Context, ip net.IP, port int, fd in
 		log.Printf("fdConn FileConn Close Fd: %d Err: %v", fd, err)
 		return nil, err
 	}
+	conn.SetDeadline(time.Now().Add(time.Second * 16))
 
 	return conn, nil
 }
