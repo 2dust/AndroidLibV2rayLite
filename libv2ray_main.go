d511441a3750daec63955e92d9f5eaedf082b0c8
Pk-web6936 <202365630+Pk-web6936@users.noreply.github.com>
2025-04-03 14:44:46 +0330

    Revert Update init with xudp basekey
    https://github.com/2dust/AndroidLibV2rayLite/pull/48/commits/c8ef47b4764ed7d4c394e2b20d8fc2117cb96826

--- libv2ray_main.go
+++ libv2ray_main.go
@@ -184,15 +184,12 @@ func (v *V2RayPoint) MeasureDelay(url string) (int64, error) {
 }
 
 // InitV2Env set v2 asset path
-func InitV2Env(envPath string, key string) {
+func InitV2Env(envPath string) {
 	//Initialize asset API, Since Raymond Will not let notify the asset location inside Process,
 	//We need to set location outside V2Ray
 	if len(envPath) > 0 {
 		os.Setenv(v2Asset, envPath)
 	}
-        if len(key) > 0 {
- 		os.Setenv(xudpBaseKey, key)
- 	}
 	
 	//Now we handle read, fallback to gomobile asset (apk assets)
 	v2filesystem.NewFileReader = func(path string) (io.ReadCloser, error) {
